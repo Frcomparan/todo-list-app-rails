0252420922fc2d85c7527eb606b34e873e2bf5a7
Frcomparan <frcomparan@gmail.com>
2022-11-07 18:06:31 -0600

    Add media queries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def users_options
+  end
 
+  def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :tasks
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'tasks#index'
+  root 'pages#home'
 
   get 'users_options' => 'pages#users_options'
 end
