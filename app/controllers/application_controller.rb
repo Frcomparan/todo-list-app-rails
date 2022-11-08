6ca98f123b223d16bb1bbf3da5774d0680c0e8b8
Frcomparan <frcomparan@gmail.com>
2022-11-08 11:02:04 -0600

    Update styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :set_nav_option
 
   def set_locale
     I18n.locale = 'es'
+  end
+
+  def set_nav_option
+    @option = ''
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def users_options
+    @option = 'user'
   end
 
   def home
+    @option = 'home'
   end
 end
